1fbc61e9433acff6d19e5daef61a01083151bc1e
Hara <harachung90@gmail.com>
2022-08-10 20:25:07 +1000

    Third commit. Filling in the methods now... (2)

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -53,6 +53,16 @@ public class GamePanel extends JPanel implements ActionListener {
         }
         g.setColor(Color.red);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+        for (int i = 0; i<bodyParts; i++) {
+            if (i == 0) {
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            } else {
+                g.setColor(new Color(45, 180, 0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
     }
 
     public void newApple() {
@@ -88,7 +98,32 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void checkCollisions(){
+        //checks if head collides with body
+        for (int i = bodyParts; i>0; i--) {
+            if ((x[0] == x[i]) && (y[0] == y[i])) {
+                running = false;
+            }
+        }
+        //checks if head touches left border
+        if (x[0] < 0) {
+            running = false;
+        }
+        //checks if head touches right border
+        if (x[0]> SCREEN_WIDTH) {
+            running = false;
+        }
+        //checks if head touches top border
+        if (y[0] < 0) {
+            running = false;
+        }
+        //checks if head touches bottom border
+        if (y[0] > SCREEN_HEIGHT) {
+            running = false;
+        }
 
+        if (!running) {
+            timer.stop();
+        }
     }
 
     public void gameOver(Graphics g) {
@@ -98,13 +133,39 @@ public class GamePanel extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if (running) {
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
     }
 
     public class MyKeyAdapter extends KeyAdapter {
        //@Override
         public void KeyPressed(KeyEvent e) {
-
+            switch(e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    if(direction != 'R') {
+                        direction = 'L';
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if(direction != 'L') {
+                        direction = 'R';
+                    }
+                    break;
+                case KeyEvent.VK_UP:
+                    if(direction != 'D') {
+                        direction = 'U';
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if(direction != 'U') {
+                        direction = 'D';
+                    }
+                    break;
+            }
         }
     }
 }
